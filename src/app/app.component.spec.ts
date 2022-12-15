ce09e8d7bd7da1ab185a5293e83e5fb4f04d1f73
Hiruurouge <hiruurouge@gmail.com>
2022-12-15 09:48:12 +0100

    ng new

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'agileProject'`, () => {
+  it(`should have as title 'Agile'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('agileProject');
+    expect(app.title).toEqual('Agile');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('agileProject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Agile app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccueilComponent } from './pages/accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AccueilComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
